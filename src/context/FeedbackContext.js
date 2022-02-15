1d8520ee60775b960e2f01710a8c59854a589ea5
Adrian <achapa@adrianchapa.dev>
2022-02-15 04:13:37 -0600

    Update and Delete from json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -40,14 +40,24 @@ export const FeedbackProvider = ({ children }) => {
     const response = await fetch('/feedback?_sort=id&_order=desc');
     console.log(response);
     const data = await response.json();
+    // load state with data fetched from backend
     setFeedback(data);
     setIsLoading(false);
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
     setFeedbackEdit({
       item: {},
@@ -64,8 +74,9 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
